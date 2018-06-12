6e1c29c042ab51fda3ccd9addaa1c9bd7e2e632d
kodingattitude <KASANI@DESKTOP-AG1QGVH>
2018-06-12 05:37:01 +0530

    clear data in input controls in angularjs

---
+++ AngularJS-ClearInputControls/index.js
@@ -0,0 +1,30 @@
+﻿var app = angular.module('angularclearinputcontrols', ['ngMessages']);
+app.controller('ClearInputControlsController', function ($scope, $window) {
+
+    $scope.SubmitForm = function () {
+
+        if ($scope.formvalidation.$valid) {
+            $scope.submitted = false;
+            alert("Submitted!!");
+        }
+        else {
+            //if $scope.formvalidation.$valid is not true then it submitted= true i.e., the submitted attribute should work
+            $scope.submitted = true;
+        }
+
+    }
+    $scope.ClearDataInInputControls = function () {
+        $scope.FormInfo = {};
+    }
+    $scope.ClearByReload = function () {
+        $window.location.reload();
+
+    }
+    $scope.ClearByReset = function () {
+        $scope.formvalidation.$setPristine();  // it sets the form to pristine
+        $scope.formvalidation.$setUntouched(); // it sets the form to untouched
+        // $scope.formvalidation.$submitted = false;  // we can use this in place of $scope.submitted
+        $scope.submitted = false;
+        $scope.FormInfo = {}; // Clear the ng-model object that holds the ng-model value
+    }
+});
